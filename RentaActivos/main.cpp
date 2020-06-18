cd38354643280e3b7d47beef36bcbe2567e6e94b
danisotz <danisotz@gmail.com>
2020-06-18 08:12:10 -0600

    Ingreso de usuarios

--- RentaActivos/main.cpp
+++ RentaActivos/main.cpp
@@ -34,6 +34,18 @@ string generarCodigo()
     return codigo;
 }
 
+string mayusculas(string cadena)
+{
+
+    for(int i=0; i< cadena.length();i++)
+    {
+        cadena[i]= toupper(cadena[i]);
+    }
+
+    return cadena;
+
+}
+
 int main()
 {
     bool men = true;
@@ -52,19 +64,10 @@ int main()
         string opcionjug2="";
         string usuario, contra, nombre,depar,empre;
 
-
-
-        string ur = "";
         system("cls");
-        cout << "***********RENTA DE ACTIVOS***********\n\n";
-        cout << "1. Agregar nuevo usuario" << endl;
-        cout << "2. Scoreboard" << endl;
-        cout << "3. Iniciar juego" << endl;
-        cout << "4. Lectura de Archivo" << endl;
-        cout << "5. Reports" << endl;
-        cout << "6. Exit" << endl;
-        cout<< "Codigo: " << generarCodigo()<< endl;
-
+        cout << "-------------- RENTA DE ACTIVOS -------------"<< endl;
+        cout << "1. Iniciar sesion" << endl;
+        cout << "2. Exit" << endl;
         cout << "Selecciona una opcion\n"; cin >> opcion;
         cout << "opcion: " << opcion << endl;
 
@@ -72,25 +75,103 @@ int main()
         {
         case 1:
             system("cls");
-            cout << "---------- NUEVO USUARIO------------"<< endl;
-            cout << "Ingrese nombre usuario"<< endl;
+            cout << "-------------- Renta de Activos -------------"<< endl;
+            cout << "--------------- INICIAR SESION --------------"<< endl;
+            cout << "Ingrese usuario"<< endl;
             cin >> usuario;
             cout << "Ingrese contraseña"<< endl;
             cin >> contra;
-            cout << "Ingrese nombre completo"<< endl;
-            cin >> nombre;
             cout << "Ingrese Departamento"<< endl;
             cin >> depar;
             cout << "Ingrese Empresa"<< endl;
             cin >> empre;
-            existe= mat->buscarUsuario(depar,empre,usuario);
-            if(existe)
-                cout << "Usuario ya existe en esa empresa y departamento"<< endl;
+            depar = mayusculas(depar);
+            empre = mayusculas(empre);
+            if(usuario.compare("admin")==0)
+            {
+                if(contra.compare("123")==0)
+                {
+                    men2=true;
+                    while(men2)
+                    {
+                        system("cls");
+                        cout << "-------------- Renta de Activos -------------"<< endl;
+                        cout << "--------------- Menu Administrador --------------"<< endl;
+                        cout << "1. Registrar Usuario"<< endl;
+                        cout << "2. Reporte Matriz Dispersa"<< endl;
+                        cout << "3. Reporte Activos Disponibles de un Departamento"<< endl;
+                        cout << "4. Reporte Activos Disponibles de una Empresa"<< endl;
+                        cout << "5. Reporte Transacciones"<< endl;
+                        cout << "6. Reporte Activos de un Usuario"<< endl;
+                        cout << "7. Activos rentados por un Usuario"<< endl;
+                        cout << "8. Ordenar Transacciones"<< endl;
+                        cout << "9. Salir"<< endl;
+                        cout << "Selecciona una opcion\n"; cin >> opcion;
+                        cout << "opcion: " << opcion << endl;
+                        switch(opcion)
+                        {
+                        case 1:
+                            system("cls");
+                            cout << "---------- NUEVO USUARIO------------"<< endl;
+                            cout << "Ingrese nombre usuario"<< endl;
+                            cin >> usuario;
+                            cout << "Ingrese contraseña"<< endl;
+                            cin >> contra;
+                            cout << "Ingrese nombre completo"<< endl;
+                            cin >> nombre;
+                            cout << "Ingrese Departamento"<< endl;
+                            cin >> depar;
+                            cout << "Ingrese Empresa"<< endl;
+                            cin >> empre;
+                            depar = mayusculas(depar);
+                            empre = mayusculas(empre);
+                            existe= mat->buscarUsuario(depar,empre,usuario);
+                            if(existe)
+                                cout << "Usuario ya existe en esa empresa y departamento"<< endl;
+                            else
+                            {
+                                mat->insertar(depar,empre,usuario,contra,nombre);
+                                _getch();
+                                arb->insertar(usuario,contra,nombre);
+                                _getch();
+                            }
+                            _getch();
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            break;
+                        case 7:
+                            break;
+                        case 8:
+                            break;
+                        default:
+                            men2=false;
+                            break;
+                        }
+                    }
+
+                }
+
+            }
+
             else
-                mat->insertar(depar,empre,usuario,contra,nombre);
-                arb->insertar(usuario,contra,nombre);
+            {
+                existe= mat->buscarUsuario(depar,empre,usuario);
+                if(existe)
+                    cout << "Usuario ingresa"<< endl;
+                else
+                    cout << "Usuario no esta registrado"<< endl;
+            }
             _getch();
             break;
+
         case 2:
             system("cls");
             cout << "-------------- Renta de Activos -------------"<< endl;
@@ -119,6 +200,25 @@ int main()
             _getch();
             break;
         case 3:
+            system("cls");
+            cout << "---------- NUEVO USUARIO------------"<< endl;
+            cout << "Ingrese nombre usuario"<< endl;
+            cin >> usuario;
+            cout << "Ingrese contraseña"<< endl;
+            cin >> contra;
+            cout << "Ingrese nombre completo"<< endl;
+            cin >> nombre;
+            cout << "Ingrese Departamento"<< endl;
+            cin >> depar;
+            cout << "Ingrese Empresa"<< endl;
+            cin >> empre;
+            existe= mat->buscarUsuario(depar,empre,usuario);
+            if(existe)
+                cout << "Usuario ya existe en esa empresa y departamento"<< endl;
+            else
+                mat->insertar(depar,empre,usuario,contra,nombre);
+                arb->insertar(usuario,contra,nombre);
+
 
             ambos=true;
             activo=true;
@@ -136,7 +236,6 @@ int main()
             break;
         case 4:
             cout << "Ingrese url de Json\n";
-            cin >> ur;
              _getch();
             break;
         case 5:
